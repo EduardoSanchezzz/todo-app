130123a97518818c32e66d861ebcd1264260dd88
Eduardo Sanchez <e5sanche@uwaterloo.ca>
2020-09-20 18:57:59 -0400

    First Version of TodoList WebApp

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function About() {
+  return(
+    <React.Fragment>
+      <h1> About </h1>
+      <p> React Crash Course </p>
+
+    </React.Fragment>
+
+  )
+}
+
+export default About;
