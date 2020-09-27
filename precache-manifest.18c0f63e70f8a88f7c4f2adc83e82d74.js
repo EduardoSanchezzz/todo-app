f5588283c52ce3a5d0749e76201933751cd6ce9f
esanchezz74 <esanch747@gmail.com>
2020-09-27 00:46:49 -0400

    Updates

---
+++ precache-manifest.18c0f63e70f8a88f7c4f2adc83e82d74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa982bb2c95bf86009d67634e5b5a126",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "e867c0ad830da4a20dd0",
+    "url": "/todo-app/static/css/main.2daee2a7.chunk.css"
+  },
+  {
+    "revision": "87d6fd225cdc62a008a1",
+    "url": "/todo-app/static/js/2.865b34cd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-app/static/js/2.865b34cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e867c0ad830da4a20dd0",
+    "url": "/todo-app/static/js/main.ea0eae9c.chunk.js"
+  },
+  {
+    "revision": "150d6b1c4ff25acc2e85",
+    "url": "/todo-app/static/js/runtime-main.bd94374d.js"
+  }
+]);
